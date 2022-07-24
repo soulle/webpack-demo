669a00da2915fd8ff2b733a883cc91bafe0ca1cc
Saule <saulekazanbaeva3110@gmail.com>
2022-07-24 20:55:02 +0300

    add babel, js hash and source-map

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,11 @@ console.log(mode + " mode");
 module.exports = {
   mode: mode,
   output: {
+    filename: "[name].[contenthash].js",
     assetModuleFilename: "assets/[hash][ext][query]",
     clean: true,
   },
+  devtool: "source-map",
   plugins: [
     new MiniCssExtractPlugin({
       filename: "[name].[contenthash].css",
@@ -62,6 +64,16 @@ module.exports = {
         test: /\.pug$/,
         loader: "pug-loader",
         exclude: /(node_modules|bower_components)/,
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
     ],
   },
